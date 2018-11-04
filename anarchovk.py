7ff019e0f235cdec33299546e7c70fe1dd5bbddb
dezzev <44578939+dezzev@users.noreply.github.com>
2018-11-05 00:06:52 +0500

    Update anarchovk.py

--- anarchovk.py
+++ anarchovk.py
@@ -7,6 +7,6 @@ def auth(my_token):
     token = my_token
 
 def method(my_method, method_params):
-    method_dict = dict({"version": "5.87", "access_token": token}.items() | method_params.items()) 
+    method_dict = dict({"v": "5.87", "access_token": token}.items() | method_params.items()) 
     method_get = requests.get("https://api.vk.com/method/" + my_method, params=method_dict).json()
     return(method_get)
